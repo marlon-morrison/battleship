96ac4badac4b03d49f06b6a8cf11ca977536b456
marlon-morrison <52177393+marlon-morrison@users.noreply.github.com>
2019-12-19 14:02:57 -0600

    small changes

--- js/app.js
+++ js/app.js
@@ -1,58 +1,10 @@
-let gamebored = document.getElementById("gameboard")
-let win = false 
-let tries = 5
+let gamebored = document.getElementById("gameboard");
+let turnsLeft = document.getElementById("turnsLeft")
+let win = false ;
+let tries = 8;
 // Board
-const ships = [
-    {
-        position:0
-    },
-    {
-        position:1
-    },
-    {
-        position:2
-    },
-    {
-        position:3
-    },
-    {
-        position:4
-    },
-    {
-        position:5
-    },
-    {
-        position:6
-    },
-    {
-        position:7
-    },
-    {
-        position:8
-    },
-    {
-        position:9
-    },
-    {
-        position:10
-    },
-    {
-        position:11
-    },
-    {
-        position:12
-    },
-    {
-        position:13
-    },
-    {
-        position:14
-    },
-    {
-        position:15
-    },
-];
-for (let i = 0; i < ships.length; i++) {
+
+for (let i = 0; i < 40; i++) {
     var ele = document.createElement("div");
     ele.classList.add("col-sm-3","letter");
     // ele.style.background = "lightblue";
@@ -63,13 +15,12 @@ for (let i = 0; i < ships.length; i++) {
     });
 };
 // Computer move
-let computer = ships[Math.floor(Math.random()*Math.floor(ships.length))];
-console.log(computer);
+let computer = Math.floor(Math.random()*Math.floor(20));
 // Game play
 
 function player(tile) {
     var value = tile.getAttribute('data-value');
-    var gameover = document.getElementById("gameover")
+    var gameover = document.getElementById("gameover");
     if (!win) {
         if (tries < 1) {
             win = true
@@ -77,20 +28,19 @@ function player(tile) {
             gameover.classList.add("gameover")
         }
         
-        if (value == computer.position) {
+        if (value == computer) {
             console.log("win");
             tile.style.background = "green"
             gameover.innerText = "Game Over"
             gameover.classList.add("gameover")
             win = true
         } else {
-            tries--
+            turnsLeft.innerText = `You have ${tries--} turns left until your ship escape.`
             console.log("miss");
             tile.style.background = "#880000"
         };
         // debugger
     }
-    
     return false;
 };
 // Reset
